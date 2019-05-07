1c4f8e15b59fff2e426efd90c42272031d869e0f
jhoicas <jhoicas@gmail.com>
2019-05-07 14:12:33 -0500

    :-) Butons

--- firstappreact/src/pages/About/About.js
+++ firstappreact/src/pages/About/About.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from '../Home/Home';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 function About() {
+    const [arrozchino, setArrozchino] = useState(0);
     return (<div> 
-    <Home /> <button><Link to="/" className="navbar-brand"> Home</Link></button></div>
+    <Home /> <button><Link to="/" className="navbar-brand"> Home</Link></button>
+    
+    <p>Ud ha tocado el boton {arrozchino}</p>
+    <button onClick={()=> setArrozchino(arrozchino+1)}>Click me</button>
+    </div>
     
     );
 }
